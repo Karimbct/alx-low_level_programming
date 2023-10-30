f2dc26ca8ca074dfce64490ba74e3c340143facc
Karimbct <karimbelouche@gmail.com>
2023-10-30 08:42:23 -0700

    File io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the lettres
+ * @filename: the name of file.
+ * @letters: the number of letters.
+ * Return:the  numbers of letters printed fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	char *buffer;
+	ssize_t bytes_written, bytes_read;
+
+	if (!filename)
+		return (0);
+
+	file_descriptor = open(filename, O_RDONLY);
+
+	if (file_descriptor == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (buffer)
+		return (0);
+
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	close(file_descriptor);
+
+	free(buffer);
+
+	return (bytes_written);
+}
