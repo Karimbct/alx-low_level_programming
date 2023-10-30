f6fbef574dcb366d8fa959b9ad7935b6484b861f
Karimbct <karimbelouche@gmail.com>
2023-10-30 09:21:06 -0700

    File io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	
+
 	if (file_descriptor == -1)
 		return (-1);
 
